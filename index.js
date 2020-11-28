0c8555fe945f4ba8da7d307737881ec085983c2a
Raphael Limbikani Mbewe <rmbewe68@gmail.com>
2020-11-28 08:26:11 +0000

    corrected some texts in the about section

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var p = document.getElementById('text');
 p.innerHTML = '';
 var n = 0;
-var str = 'My full names are Raphael Limbikani Mbewe I\'m a junior software developer in web and mobile applictions development,currently living in Zambia.I\'m looking forward to collaborate with people on different projects,Seeking for growth in my tech career that\'s my main goal,I love being a team player I am a strong believer of team work, respectful and believes in all my team mates. I am open for any project  provided it gives me growth in a positve way.';
+var str = 'My full names are Raphael Limbikani Mbewe I\'m a junior software developer in web and mobile applications development, currently living in Zambia. I\'m looking forward to collaborating with people on different projects, Seeking growth in my tech career that\'s my main goal, I love being a team player I am a strong believer in teamwork, respectful, and believes in all my teammates. I am open to any project provided it gives me growth in a positive way';
 var typeTimer = setInterval(function() {
   n = n + 1;
   p.innerHTML = " " + str.slice(0, n);
